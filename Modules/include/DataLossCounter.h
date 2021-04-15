cfb73d8daea5db409e091db05379d8842f52cd61
Martin Hierholzer <martin.hierholzer@desy.de>
2021-04-15 16:57:09 +0200

    DataLossCounter constructor now takes qualified path to trigger variable

--- Modules/include/DataLossCounter.h
+++ Modules/include/DataLossCounter.h
@@ -6,6 +6,7 @@
 #include <ChimeraTK/SupportedUserTypes.h>
 
 #include "ApplicationCore.h"
+#include "HierarchyModifyingGroup.h"
 
 namespace ChimeraTK {
 
@@ -16,9 +17,47 @@ namespace ChimeraTK {
    */
   template<typename TRIGGERTYPE = int32_t>
   struct DataLossCounter : ApplicationModule {
-    using ApplicationModule::ApplicationModule;
+    /**
+     *  Construct a DataLossCounter object.
+     *
+     *  pathToTrigger is a qualified name of the trigger source. It should start with "/" or ".." to denote an absolute
+     *  resp. relative path. Note that relative paths are relative to the DataLossCounter itself.
+     */
+    DataLossCounter(EntityOwner* owner, const std::string& name, const std::string& description,
+                    const std::string &pathToTrigger,
+                    HierarchyModifier hierarchyModifier = HierarchyModifier::none,
+                    const std::unordered_set<std::string>& tags = {})
+     : ApplicationModule(owner, name, description, hierarchyModifier, tags),
+       triggerGroup(this, pathToTrigger), trigger(triggerGroup.trigger)
+    {}
 
-    ScalarPushInput<TRIGGERTYPE> trigger{this, "trigger", "", "Trigger input"};
+    /// Deprecated form of the constructor for backwards compatibility only.
+    [[deprecated]] DataLossCounter(EntityOwner* owner, const std::string& name, const std::string& description,
+                    HierarchyModifier hierarchyModifier = HierarchyModifier::none,
+                    const std::unordered_set<std::string>& tags = {})
+     : ApplicationModule(owner, name, description, hierarchyModifier, tags),
+       triggerGroup_compat(this, "TriggerGroup", "", HierarchyModifier::hideThis), trigger(triggerGroup_compat.trigger)
+    {}
+
+    DataLossCounter() {}
+
+    struct TriggerGroup : HierarchyModifyingGroup {
+      TriggerGroup(EntityOwner* owner, const std::string& pathToTrigger)
+      : HierarchyModifyingGroup(owner, HierarchyModifyingGroup::getPathName(pathToTrigger), ""),
+        trigger{this,  HierarchyModifyingGroup::getUnqualifiedName(pathToTrigger), "", "Trigger input"} {}
+
+      TriggerGroup() {}
+
+      ScalarPushInput<TRIGGERTYPE> trigger;
+    } triggerGroup;
+
+    struct TriggerGroup_compat : VariableGroup {
+      using VariableGroup::VariableGroup;
+      ScalarPushInput<TRIGGERTYPE> trigger{this, "trigger", "", "Trigger input"};
+    } triggerGroup_compat;
+
+    // This is for backwards compatibility!
+    ScalarPushInput<TRIGGERTYPE> &trigger;
 
     ScalarOutput<uint64_t> lostDataInLastTrigger{this, "lostDataInLastTrigger", "",
         "Number of data transfers during "
