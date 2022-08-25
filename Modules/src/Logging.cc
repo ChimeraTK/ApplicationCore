ChimeraTK/ApplicationCore

commit 9d53994cd1d75501c3b4379159a2a096ee386586

Remove unused parameter

Modules/src/Logging.cc
@@ -259,7 +259,7 @@ void LoggingModule::mainLoop() {
   }
 }
 
-ctk::VariableNetworkNode LoggingModule::getAccessorPair(const ctk::RegisterPath& namePrefix, const std::string& name) {
+ctk::VariableNetworkNode LoggingModule::getAccessorPair(const ctk::RegisterPath& namePrefix) {
   auto it = std::find_if(sources.begin(), sources.end(),
       boost::bind(&MessageSource::sendingModule, boost::placeholders::_1) == (std::string)namePrefix);
   if(it == sources.end()) {

Modules/include/Logging.h
@@ -211,7 +211,7 @@ namespace logging {
    */
   class LoggingModule : public ctk::ApplicationModule {
    private:
-    ctk::VariableNetworkNode getAccessorPair(const ctk::RegisterPath& namePrefix, const std::string& name);
+    ctk::VariableNetworkNode getAccessorPair(const ctk::RegisterPath& namePrefix);
 
     /** Map of VariableGroups required to build the hierarchies. The key it the
      * full path name. */

commit c7c69c0321e9b4d93edc9c9d9f48ebdceaf156be

Fix bug introduced with PR #245

Modules/src/Logging.cc
@@ -92,7 +92,7 @@ LoggingModule::LoggingModule(ctk::EntityOwner* owner, const std::string& name, c
         auto msgSource = std::find(sources.begin(), sources.end(), moduleName);
         if(msgSource == sources.end()) {
           // Create new MessageSource
-          auto acc = getAccessorPair(moduleName, it->getOwningModule()->getName());
+          auto acc = getAccessorPair(moduleName);
           (*it) >> acc;
           std::cout << "Registered module " << it->getOwningModule()->getQualifiedName() << " for logging."
                     << std::endl;