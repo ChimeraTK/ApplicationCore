69a3452f70ea665089cac34641fc0b22ba4589a6
Tomasz Kozak <tomasz.kozak@desy.de>
2022-08-31 16:25:02 +0200

    Issue:8279 - Remove implementation with the old interface, adjusts tests.

--- Modules/include/StatusMonitor.h
+++ Modules/include/StatusMonitor.h
@@ -34,178 +34,20 @@ For more info see \ref statusmonitordoc
 #include "HierarchyModifyingGroup.h"
 #include "ScalarAccessor.h"
 #include "StatusAccessor.h"
-#include "VariableGroup.h"
 
 namespace ChimeraTK {
 
-  template<typename T>
-  struct StatusMonitor : public ApplicationModule {
-    /** Number of convience constructors for ease of use.
-     * The input and output variable names can be given by user which
-     * should be mapped with the variables of module to be watched.
-     */
-    StatusMonitor(EntityOwner* owner, const std::string& name, const std::string& description, const std::string& input,
-        const std::string& output, HierarchyModifier modifier, const std::unordered_set<std::string>& outputTags = {},
-        const std::unordered_set<std::string>& parameterTags = {}, const std::unordered_set<std::string>& tags = {})
-    : ApplicationModule(owner, name, description, modifier, tags), _parameterTags(parameterTags), _input(input),
-      status(this, output, "", outputTags), oneUp(this, input) {}
-
-    StatusMonitor() { throw logic_error("Default constructor unusable. Just exists to work around gcc bug."); }
-    StatusMonitor(StatusMonitor&&) = default;
-
-    ~StatusMonitor() override {}
-    void prepare() override {}
-
-    /**Tags for parameters. This makes it easier to connect them to e.g, to control system*/
-    std::unordered_set<std::string> _parameterTags;
-
-    const std::string _input;
-
-    /** Status to be reported */
-    StatusOutput status;
-
-    /** Disable the monitor. The status will always be OFF. You don't have to connect this input.
-     *  When there is no feeder, ApplicationCore will connect it to a constant feeder with value 0, hence the monitor is
-     * always enabled.
-     */
-    ScalarPushInput<int> disable{this, "disable", "", "Disable the status monitor"};
-
-    /**Input value that should be monitored. It is moved one level up, so it's parallel to this monitor object.*/
-    struct OneUp : public VariableGroup {
-      OneUp(EntityOwner* owner, const std::string& watchName)
-      : VariableGroup(owner, "hidden", "", HierarchyModifier::oneUpAndHide), watch(this, watchName, "", "") {}
-      ScalarPushInput<T> watch;
-    } oneUp;
-  };
-
   /** Module for status monitoring depending on a maximum threshold value*/
   template<typename T>
-  struct MaxMonitor : public StatusMonitor<T> {
-    using StatusMonitor<T>::StatusMonitor;
-    /** WARNING state to be reported if threshold is reached or exceeded*/
-    ScalarPushInput<T> warning{this, "upperWarningThreshold", "", "", StatusMonitor<T>::_parameterTags};
-    /** FAULT state to be reported if threshold is reached or exceeded*/
-    ScalarPushInput<T> fault{this, "upperFaultThreshold", "", "", StatusMonitor<T>::_parameterTags};
-
-    /**This is where state evaluation is done*/
-    void mainLoop() {
-      /** If there is a change either in value monitored or in thershold values, the status is re-evaluated*/
-      ReadAnyGroup group{StatusMonitor<T>::oneUp.watch, StatusMonitor<T>::disable, warning, fault};
-      while(true) {
-        // evaluate and publish first, then read and wait. This takes care of the publishing the initial variables
-        if(StatusMonitor<T>::disable != 0) {
-          StatusMonitor<T>::status = StatusOutput::Status::OFF;
-        }
-        else if(StatusMonitor<T>::oneUp.watch >= fault) {
-          StatusMonitor<T>::status = StatusOutput::Status::FAULT;
-        }
-        else if(StatusMonitor<T>::oneUp.watch >= warning) {
-          StatusMonitor<T>::status = StatusOutput::Status::WARNING;
-        }
-        else {
-          StatusMonitor<T>::status = StatusOutput::Status::OK;
-        }
-        StatusMonitor<T>::status.write();
-        group.readAny();
-      }
-    }
-  };
-
-  /** Module for status monitoring depending on a minimum threshold value*/
-  template<typename T>
-  struct MinMonitor : public StatusMonitor<T> {
-    using StatusMonitor<T>::StatusMonitor;
-
-    /** WARNING state to be reported if threshold is crossed*/
-    ScalarPushInput<T> warning{this, "lowerWarningThreshold", "", "", StatusMonitor<T>::_parameterTags};
-    /** FAULT state to be reported if threshold is crossed*/
-    ScalarPushInput<T> fault{this, "lowerFaultThreshold", "", "", StatusMonitor<T>::_parameterTags};
-
-    /**This is where state evaluation is done*/
-    void mainLoop() {
-      /** If there is a change either in value monitored or in thershold values, the status is re-evaluated*/
-      ReadAnyGroup group{StatusMonitor<T>::oneUp.watch, StatusMonitor<T>::disable, warning, fault};
-      while(true) {
-        if(StatusMonitor<T>::disable != 0) {
-          StatusMonitor<T>::status = StatusOutput::Status::OFF;
-        }
-        else if(StatusMonitor<T>::oneUp.watch <= fault) {
-          StatusMonitor<T>::status = StatusOutput::Status::FAULT;
-        }
-        else if(StatusMonitor<T>::oneUp.watch <= warning) {
-          StatusMonitor<T>::status = StatusOutput::Status::WARNING;
-        }
-        else {
-          StatusMonitor<T>::status = StatusOutput::Status::OK;
-        }
-        StatusMonitor<T>::status.write();
-        group.readAny();
-      }
-    }
-  };
-
-  /** Module for status monitoring depending on range of threshold values.
-   * As long as a monitored value is in the range defined by user it goes
-   * to fault or warning state. If the monitored value exceeds the upper limmit
-   * or goes under the lowerthreshold the state reported will be always OK.
-   * IMPORTANT: This module does not check for ill logic, so make sure to
-   * set the ranges correctly to issue warning or fault.
-   */
-  template<typename T>
-  struct RangeMonitor : public StatusMonitor<T> {
-    using StatusMonitor<T>::StatusMonitor;
-
-    /** WARNING state to be reported if value is in between the upper and
-     * lower threshold including the start and end of thresholds.
-     */
-    ScalarPushInput<T> warningUpperThreshold{this, "upperWarningThreshold", "", "", StatusMonitor<T>::_parameterTags};
-    ScalarPushInput<T> warningLowerThreshold{this, "lowerWarningThreshold", "", "", StatusMonitor<T>::_parameterTags};
-    /** FAULT state to be reported if value is in between the upper and
-     * lower threshold including the start and end of thresholds.
-     */
-    ScalarPushInput<T> faultUpperThreshold{this, "upperFaultThreshold", "", "", StatusMonitor<T>::_parameterTags};
-    ScalarPushInput<T> faultLowerThreshold{this, "lowerFaultThreshold", "", "", StatusMonitor<T>::_parameterTags};
-
-    /**This is where state evaluation is done*/
-    void mainLoop() {
-      /** If there is a change either in value monitored or in thershold values, the status is re-evaluated*/
-      ReadAnyGroup group{StatusMonitor<T>::oneUp.watch, StatusMonitor<T>::disable, warningUpperThreshold,
-          warningLowerThreshold, faultUpperThreshold, faultLowerThreshold};
-      while(true) {
-        if(StatusMonitor<T>::disable != 0) {
-          StatusMonitor<T>::status = StatusOutput::Status::OFF;
-        }
-        // Check for fault limits first. Like this they supersede the warning,
-        // even if they are stricter then the warning limits (mis-configuration)
-        else if(StatusMonitor<T>::oneUp.watch <= faultLowerThreshold ||
-            StatusMonitor<T>::oneUp.watch >= faultUpperThreshold) {
-          StatusMonitor<T>::status = StatusOutput::Status::FAULT;
-        }
-        else if(StatusMonitor<T>::oneUp.watch <= warningLowerThreshold ||
-            StatusMonitor<T>::oneUp.watch >= warningUpperThreshold) {
-          StatusMonitor<T>::status = StatusOutput::Status::WARNING;
-        }
-        else {
-          StatusMonitor<T>::status = StatusOutput::Status::OK;
-        }
-        StatusMonitor<T>::status.write();
-        group.readAny();
-      }
-    }
-  };
-
-  // NEW INTERFACE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  template<typename T>
-  struct MaxMonitor2 : ApplicationModule {
-    MaxMonitor2(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
+  struct MaxMonitor : ApplicationModule {
+    MaxMonitor(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
         const std::string& parameterPath, const std::string& description,
         const std::unordered_set<std::string>& outputTags = {},
         const std::unordered_set<std::string>& parameterTags = {})
-    : MaxMonitor2(owner, inputPath, outputPath, parameterPath + "/upperWarningThreshold",
+    : MaxMonitor(owner, inputPath, outputPath, parameterPath + "/upperWarningThreshold",
           parameterPath + "/upperFaultThreshold", parameterPath + "/disable", description, outputTags, parameterTags) {}
 
-    MaxMonitor2(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
+    MaxMonitor(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
         const std::string& warningThreholdPath, const std::string& faultThreholdPath, const std::string& disablePath,
         const std::string& description, const std::unordered_set<std::string>& outputTags = {},
         const std::unordered_set<std::string>& parameterTags = {})
@@ -216,7 +58,7 @@ namespace ChimeraTK {
       disable(this, disablePath, "", "Disable the status monitor", parameterTags),
       status(this, outputPath, "Resulting status", outputTags) {}
 
-    MaxMonitor2() = default;
+    MaxMonitor() = default;
 
     /** Variable to monitor */
     ModifyHierarchy<ScalarPushInput<T>> watch;
@@ -226,6 +68,11 @@ namespace ChimeraTK {
 
     /** FAULT state to be reported if threshold is reached or exceeded*/
     ModifyHierarchy<ScalarPushInput<T>> faultThreshold;
+
+    /** Disable the monitor. The status will always be OFF. You don't have to connect this input.
+     *  When there is no feeder, ApplicationCore will connect it to a constant feeder with value 0, hence the monitor is
+     * always enabled.
+     */
 
     /** Disable/enable the entire status monitor */
     ModifyHierarchy<ScalarPushInput<int>> disable;
@@ -267,16 +114,17 @@ namespace ChimeraTK {
     }
   };
 
+  /** Module for status monitoring depending on a minimum threshold value*/
   template<typename T>
-  struct MinMonitor2 : ApplicationModule {
-    MinMonitor2(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
+  struct MinMonitor : ApplicationModule {
+    MinMonitor(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
         const std::string& parameterPath, const std::string& description,
         const std::unordered_set<std::string>& outputTags = {},
         const std::unordered_set<std::string>& parameterTags = {})
-    : MinMonitor2(owner, inputPath, outputPath, parameterPath + "/lowerWarningThreshold",
+    : MinMonitor(owner, inputPath, outputPath, parameterPath + "/lowerWarningThreshold",
           parameterPath + "/lowerFaultThreshold", parameterPath + "/disable", description, outputTags, parameterTags) {}
 
-    MinMonitor2(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
+    MinMonitor(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
         const std::string& warningThreholdPath, const std::string& faultThreholdPath, const std::string& disablePath,
         const std::string& description, const std::unordered_set<std::string>& outputTags = {},
         const std::unordered_set<std::string>& parameterTags = {})
@@ -287,7 +135,7 @@ namespace ChimeraTK {
       disable(this, disablePath, "", "Disable the status monitor", parameterTags),
       status(this, outputPath, "Resulting status", outputTags) {}
 
-    MinMonitor2() = default;
+    MinMonitor() = default;
 
     /** Variable to monitor */
     ModifyHierarchy<ScalarPushInput<T>> watch;
@@ -338,17 +186,24 @@ namespace ChimeraTK {
     }
   };
 
+  /** Module for status monitoring depending on range of threshold values.
+   * As long as a monitored value is in the range defined by user it goes
+   * to fault or warning state. If the monitored value exceeds the upper limmit
+   * or goes under the lowerthreshold the state reported will be always OK.
+   * IMPORTANT: This module does not check for ill logic, so make sure to
+   * set the ranges correctly to issue warning or fault.
+   */
   template<typename T>
-  struct RangeMonitor2 : ApplicationModule {
-    RangeMonitor2(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
+  struct RangeMonitor : ApplicationModule {
+    RangeMonitor(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
         const std::string& parameterPath, const std::string& description,
         const std::unordered_set<std::string>& outputTags = {},
         const std::unordered_set<std::string>& parameterTags = {})
-    : RangeMonitor2(owner, inputPath, outputPath, parameterPath + "/lowerWarningThreshold",
+    : RangeMonitor(owner, inputPath, outputPath, parameterPath + "/lowerWarningThreshold",
           parameterPath + "/upperWarningThreshold", parameterPath + "/lowerFaultThreshold",
           parameterPath + "/upperFaultThreshold", parameterPath + "/disable", description, outputTags, parameterTags) {}
 
-    RangeMonitor2(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
+    RangeMonitor(EntityOwner* owner, const std::string& inputPath, const std::string& outputPath,
         const std::string& warningLowerThreholdPath, const std::string& warningUpperThreholdPath,
         const std::string& faultLowerThreholdPath, const std::string& faultUpperThreholdPath,
         const std::string& disablePath, const std::string& description,
@@ -364,16 +219,20 @@ namespace ChimeraTK {
       disable(this, disablePath, "", "Disable the status monitor", parameterTags),
       status(this, outputPath, "Resulting status", outputTags) {}
 
-    RangeMonitor2() = default;
+    RangeMonitor() = default;
 
     /** Variable to monitor */
     ModifyHierarchy<ScalarPushInput<T>> watch;
 
-    /** WARNING state to be reported if threshold is reached or exceeded*/
+    /** WARNING state to be reported if value is in between the upper and
+     * lower threshold including the start and end of thresholds.
+     */
     ModifyHierarchy<ScalarPushInput<T>> warningLowerThreshold;
     ModifyHierarchy<ScalarPushInput<T>> warningUpperThreshold;
 
-    /** FAULT state to be reported if threshold is reached or exceeded*/
+    /** FAULT state to be reported if value is in between the upper and
+     * lower threshold including the start and end of thresholds.
+     */
     ModifyHierarchy<ScalarPushInput<T>> faultLowerThreshold;
     ModifyHierarchy<ScalarPushInput<T>> faultUpperThreshold;
 
@@ -396,6 +255,8 @@ namespace ChimeraTK {
         if(disable.value != 0) {
           newStatus = StatusOutput::Status::OFF;
         }
+        // Check for fault limits first. Like this they supersede the warning,
+        // even if they are stricter then the warning limits (mis-configuration)
         else if(watch.value <= faultLowerThreshold.value || watch.value >= faultUpperThreshold.value) {
           newStatus = StatusOutput::Status::FAULT;
         }
