52b5b9e5d13d293e0dbd6d370b6ca7ae60b664bc
Martin Hierholzer <martin.hierholzer@desy.de>
2022-09-26 13:08:37 +0200

    move MonitorBase implementations to .cc file (#259)

--- Modules/include/StatusMonitor.h
+++ Modules/include/StatusMonitor.h
@@ -238,27 +238,6 @@ namespace ChimeraTK {
   /*******************************************************************************************************************/
 
   /*******************************************************************************************************************/
-  /* Implementation of MonitorBase ***********************************************************************************/
-  /*******************************************************************************************************************/
-  MonitorBase::MonitorBase(EntityOwner* owner, const std::string& description, const std::string& outputPath,
-      const std::string& disablePath, const std::unordered_set<std::string>& outputTags,
-      const std::unordered_set<std::string>& parameterTags)
-  : ApplicationModule(owner, "hidden", description, HierarchyModifier::hideThis),
-    disable(this, disablePath, "", "Disable the status monitor", parameterTags),
-    status(this, outputPath, "Resulting status", outputTags) {}
-
-  /*******************************************************************************************************************/
-  void MonitorBase::setStatus(StatusOutput::Status newStatus) {
-    // update only if status has changed, but always in case of initial value
-    if(status.value != newStatus || getDataValidity() != lastStatusValidity ||
-        status.value.getVersionNumber() == VersionNumber{nullptr}) {
-      status.value = newStatus;
-      status.value.write();
-      lastStatusValidity = getDataValidity();
-    }
-  }
-
-  /*******************************************************************************************************************/
   /* Implementation of MaxMonitor ************************************************************************************/
   /*******************************************************************************************************************/
   template<typename T>
