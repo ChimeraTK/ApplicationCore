9a8249f676fb21a20224f351539ee3aeaa0299d6
vaeng <34183939+vaeng@users.noreply.github.com>
2023-09-26 16:43:18 +0200

    feat: add versionInfoProvider
    * The example app has been updated to show the implementation of the
    new VersionInfoProvider module

--- example/include/ExampleApp.h
+++ example/include/ExampleApp.h
@@ -19,6 +19,7 @@
 #include <ChimeraTK/ApplicationCore/ConfigReader.h>
 #include <ChimeraTK/ApplicationCore/PeriodicTrigger.h>
 #include <ChimeraTK/ApplicationCore/ScriptedInitialisationHandler.h>
+#include <ChimeraTK/ApplicationCore/VersionInfoProvider.h>
 
 namespace ctk = ChimeraTK;
 
@@ -39,6 +40,13 @@ class ExampleApp : public ctk::Application {
   // Provide configuration constants as process variables and for use at construction phase
   // Using the application name as a base helps for automated testing against different config files.
   ctk::ConfigReader config{this, "/", getName() + "-config.xml"};
+
+  // Provide version information from the `CMakeLists.txt` as process variables
+  // Apart from the line below and the inclusion of the
+  // `#include <ChimeraTK/ApplicationCore/VersionInfoProvider.h>` line,
+  // The server is also expected to have a module named "Application"
+  // with a variable named "configPatchVersion" of type "int32" in its "config.xml" file.
+  ctk::VersionInfoProvider vip{this};
 
   // Periodic trigger used to readout data from the device periodically.
   //! [Snippet: PeriodicTrigger Instance]
