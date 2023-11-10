2fedf4afa84b041f97a5f8f5dd4a266526bd1a26
Martin Hierholzer <martin.hierholzer@desy.de>
2023-11-10 17:33:17 +0100

    chore: code cleaning StatusWithMessage

--- Modules/include/StatusWithMessage.h
+++ Modules/include/StatusWithMessage.h
@@ -16,30 +16,22 @@
 
 namespace ChimeraTK {
 
+  /********************************************************************************************************************/
+
   /**
    *  A VariableGroup for error status and message reporting.
    *  Convenience methods ensure that status and message are updated consistently.
    */
   struct StatusWithMessage : VariableGroup {
     StatusWithMessage(VariableGroup* owner, const std::string& qualifiedStatusVariableName,
-        const std::string& description = "", const std::unordered_set<std::string>& tags = {})
-    : VariableGroup(owner, Utilities::getPathName(qualifiedStatusVariableName), description, tags),
-      _status(this, Utilities::getUnqualifiedName(qualifiedStatusVariableName), description),
-      _message(this, Utilities::getUnqualifiedName(qualifiedStatusVariableName) + "_message", "", "status message") {}
+        const std::string& description = "", const std::unordered_set<std::string>& tags = {});
+
     StatusWithMessage() = default;
 
     /// to be use only for status != OK
-    void write(StatusOutput::Status status, std::string message) {
-      assert(status != StatusOutput::Status::OK);
-      _status = status;
-      _message = std::move(message);
-      writeAll();
-    }
-    void writeOk() {
-      _status = StatusOutput::Status::OK;
-      _message = "";
-      writeAll();
-    }
+    void write(StatusOutput::Status status, std::string message);
+
+    void writeOk();
 
     // FIXME: This needs additional modification in ControlSystemAdapter if changed
     // https://redmine.msktools.desy.de/issues/12241
@@ -49,39 +41,30 @@ namespace ChimeraTK {
     ScalarOutput<std::string> _message;
   };
 
+  /********************************************************************************************************************/
+
   /**
    * This is for consistent readout of StatusWithMessage - ApplicationCore version.
    *  It can be instantiated with or without message string.
    *  If instantiated without message, the message is generated automatically from the status.
    */
   struct StatusWithMessageInput : StatusWithMessageReaderBase<StatusWithMessageInput>, public VariableGroup {
+    /// Construct StatusWithMessageInput which reads only status, not message
+    StatusWithMessageInput(ApplicationModule* owner, const std::string& qualifiedName, const std::string& description,
+        const std::unordered_set<std::string>& tags = {});
+
+    /// read associated status message from given (fully qualified) msgInputName.
+    ///  If not given, it is selected automatically by the naming convention
+    void setMessageSource(std::string msgInputName = "");
+
     // TODO: This needs additional modification in ControlSystemAdapter if changed
     // https://redmine.msktools.desy.de/issues/12241
     // NOLINTNEXTLINE(readability-identifier-naming)
     StatusPushInput _status;
     // NOLINTNEXTLINE(readability-identifier-naming)
     ScalarPushInput<std::string> _message; // left uninitialized, if no message source provided
-
-    /// Construct StatusWithMessageInput which reads only status, not message
-    StatusWithMessageInput(ApplicationModule* owner, const std::string& qualifiedName, const std::string& description,
-        const std::unordered_set<std::string>& tags = {})
-    : VariableGroup(owner, Utilities::getPathName(qualifiedName), "", tags),
-      _status(this, Utilities::getUnqualifiedName(qualifiedName), description) {
-      hasMessageSource = false;
-      _statusNameLong = description;
-    }
-
-    /// read associated status message from given (fully qualified) msgInputName.
-    ///  If not given, it is selected automatically by the naming convention
-    void setMessageSource(std::string msgInputName = "") {
-      // at the time this function is called, TransferElement impl is not yet set, so don't look there for name
-      if(msgInputName.empty()) {
-        msgInputName = ((VariableNetworkNode)_status).getName() + "_message";
-      }
-      // late initialization of _message
-      _message = ScalarPushInput<std::string>(this, msgInputName, "", "");
-      hasMessageSource = true;
-    }
   };
+
+  /********************************************************************************************************************/
 
 } /* namespace ChimeraTK */
---
+++ Modules/src/StatusWithMessage.cc
@@ -0,0 +1,55 @@
+#include "StatusWithMessage.h"
+
+namespace ChimeraTK {
+
+  /********************************************************************************************************************/
+
+  StatusWithMessage::StatusWithMessage(VariableGroup* owner, const std::string& qualifiedStatusVariableName,
+      const std::string& description, const std::unordered_set<std::string>& tags)
+  : VariableGroup(owner, Utilities::getPathName(qualifiedStatusVariableName), description, tags),
+    _status(this, Utilities::getUnqualifiedName(qualifiedStatusVariableName), description),
+    _message(this, Utilities::getUnqualifiedName(qualifiedStatusVariableName) + "_message", "", "status message") {}
+
+  /********************************************************************************************************************/
+
+  void StatusWithMessage::write(StatusOutput::Status status, std::string message) {
+    assert(status != StatusOutput::Status::OK);
+    _status = status;
+    _message = std::move(message);
+    writeAll();
+  }
+
+  /********************************************************************************************************************/
+
+  void StatusWithMessage::writeOk() {
+    _status = StatusOutput::Status::OK;
+    _message = "";
+    writeAll();
+  }
+
+  /********************************************************************************************************************/
+  /********************************************************************************************************************/
+
+  StatusWithMessageInput::StatusWithMessageInput(ApplicationModule* owner, const std::string& qualifiedName,
+      const std::string& description, const std::unordered_set<std::string>& tags)
+  : VariableGroup(owner, Utilities::getPathName(qualifiedName), "", tags),
+    _status(this, Utilities::getUnqualifiedName(qualifiedName), description) {
+    hasMessageSource = false;
+    _statusNameLong = description;
+  }
+
+  /********************************************************************************************************************/
+
+  void StatusWithMessageInput::setMessageSource(std::string msgInputName) {
+    // at the time this function is called, TransferElement impl is not yet set, so don't look there for name
+    if(msgInputName.empty()) {
+      msgInputName = ((VariableNetworkNode)_status).getName() + "_message";
+    }
+    // late initialization of _message
+    _message = ScalarPushInput<std::string>(this, msgInputName, "", "");
+    hasMessageSource = true;
+  }
+
+  /********************************************************************************************************************/
+
+} // namespace ChimeraTK
