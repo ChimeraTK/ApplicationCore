dc9d89bfa4ca0ee87e109821843516ab66307b63
Jens Georg <jens.georg@desy.de>
2024-01-18 12:51:56 +0000

    fix: Do not modify model while iterating in XML generator

--- src/XMLGeneratorVisitor.cc
+++ src/XMLGeneratorVisitor.cc
@@ -32,20 +32,27 @@ namespace ChimeraTK {
   void XMLGenerator::run() {
     _rootElement->set_attribute("name", _app.getName());
 
-    std::set<Model::ProcessVariableProxy, NetworkVisitor::ProcessVariableComperator> triggers;
+    std::list<Model::DeviceModuleProxy> deviceModules;
 
-    // Collect all triggers, add a TriggerReceiver placeholder for every device associated with that trigger
-    auto triggerCollector = [&](auto proxy) {
+    // Collect all DeviceModule proxies with triggers
+    auto deviceModuleCollector = [&](auto proxy) {
       auto trigger = proxy.getTrigger();
       if(not trigger.isValid()) {
         return;
       }
 
-      triggers.insert(trigger);
+      deviceModules.push_back(proxy);
+    };
+
+    _app.getModel().visit(deviceModuleCollector, Model::depthFirstSearch, Model::keepDeviceModules);
+
+    // Add a TriggerReceiver placeholder for every device associated with that trigger
+    // Do this in two steps, otherwise we would be modifying the model while iterating
+    for(auto& proxy : deviceModules) {
+      auto trigger = proxy.getTrigger();
       VariableNetworkNode placeholder(proxy.getAliasOrCdd(), 0);
       proxy.addVariable(trigger, placeholder);
     };
-    _app.getModel().visit(triggerCollector, Model::depthFirstSearch, Model::keepDeviceModules);
 
     auto connectingVisitor = [&](auto proxy) { generateXMLNetwork(proxy); };
 
