0393dac29852971503a922bb0814b192263c3fe8
Jens Georg <jens.georg@desy.de>
2024-03-15 12:50:40 +0100

    fixup! fixup! chore: Minor linter fixes

--- tests/executables_src/testPropagateDataFaultFlag.cc
+++ tests/executables_src/testPropagateDataFaultFlag.cc
@@ -150,7 +150,7 @@ BOOST_AUTO_TEST_CASE(testDirectConnections) {
   BOOST_CHECK_EQUAL(o2[0], 10);
   BOOST_CHECK_EQUAL(o2[1], 11);
 
-  // the return channel should also receive the flag
+  // the return channel does not receive the flag
   BOOST_CHECK(i3.readNonBlocking() == false);
   BOOST_CHECK(i3.dataValidity() == ctk::DataValidity::ok);
   i3 = 20;
@@ -167,7 +167,7 @@ BOOST_AUTO_TEST_CASE(testDirectConnections) {
   BOOST_CHECK_EQUAL(o2[1], 11);
   BOOST_CHECK_EQUAL(int(i3), 10);
 
-  // clear the flag on i1, i3 will keep it for now (we have received it there and not yet sent it out!)
+  // clear the flag on i1, i3 does not get it
   i1 = 3;
   i1.setDataValidity(ctk::DataValidity::ok);
   i1.write();
@@ -188,8 +188,8 @@ BOOST_AUTO_TEST_CASE(testDirectConnections) {
   i1.setDataValidity(ctk::DataValidity::faulty);
   i1.write();
   i3 = 121;
+  i3.setDataValidity(ctk::DataValidity::faulty);
   i3.write();
-  BOOST_CHECK(i3.dataValidity() == ctk::DataValidity::ok);
   test.stepApplication();
   o1.readLatest();
   o2.readLatest();
@@ -199,7 +199,7 @@ BOOST_AUTO_TEST_CASE(testDirectConnections) {
   BOOST_CHECK_EQUAL(int(o1), 120);
   BOOST_CHECK_EQUAL(o2[0], 10);
   BOOST_CHECK_EQUAL(o2[1], 11);
-  BOOST_CHECK(i3.dataValidity() == ctk::DataValidity::ok);
+  BOOST_CHECK(i3.dataValidity() == ctk::DataValidity::faulty);
   BOOST_CHECK_EQUAL(int(i3), 10);
 
   // clear first flag
@@ -215,7 +215,7 @@ BOOST_AUTO_TEST_CASE(testDirectConnections) {
   BOOST_CHECK_EQUAL(int(o1), 122);
   BOOST_CHECK_EQUAL(o2[0], 10);
   BOOST_CHECK_EQUAL(o2[1], 11);
-  BOOST_CHECK(i3.dataValidity() == ctk::DataValidity::ok);
+  BOOST_CHECK(i3.dataValidity() == ctk::DataValidity::faulty);
   BOOST_CHECK_EQUAL(int(i3), 10);
 
   // clear second flag
