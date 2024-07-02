f4a916699d27891dd72d0c93f1c43221f3d49f3e
Martin Hierholzer <martin.hierholzer@desy.de>
2024-07-02 17:01:38 +0200

    fix(test): race condition

--- tests/deviceInitScript2.bash
+++ tests/deviceInitScript2.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 if [[ -f "produceDevice2InitError" ]]; then
-    echo Simulating error in second script
+    echo Simulating error in second script: `cat produceDevice2InitError`
     exit 1
 fi
 
+if [[ -f "produceDevice2InitSecondLine" ]]; then
+    # Create additional output line and wait until the file is removed
+    echo Just another output line...
+    while [[ -f "produceDevice2InitSecondLine" ]]; do
+        sleep 0.1
+    done
+fi
+
 echo just a second script
-       
