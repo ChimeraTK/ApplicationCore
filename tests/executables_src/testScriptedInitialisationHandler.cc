eb81711ebb6c6d1a3988f5b3991fd29c8495b0af
Martin Hierholzer <martin.hierholzer@desy.de>
2024-07-02 17:01:38 +0200

    fix(test): race condition

--- tests/executables_src/testScriptedInitialisationHandler.cc
+++ tests/executables_src/testScriptedInitialisationHandler.cc
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #define BOOST_TEST_MODULE testExceptionHandling
 #include "Application.h"
+#include "check_timeout.h"
 #include "DeviceModule.h"
 #include "ScriptedInitialisationHandler.h"
 #include "TestFacility.h"
@@ -104,33 +105,51 @@ BOOST_FIXTURE_TEST_CASE(testError, Fixture) {
   // testApp.dumpConnections();
   auto secondInitMessage = testFacility.getScalar<std::string>("/Devices/Dummy0/secondInitScriptOutput");
 
-  // let the script run three times. Afterwards manually check that there is only one printout on the console
+  // let the script run three times, check that always the output of the last run is visible in the control system
   auto startTime = std::chrono::steady_clock::now();
   for(int i = 0; i < 3; ++i) {
-    secondInitMessage.read();
-    std::string referenceString; // currently emtpy
-    BOOST_CHECK_EQUAL(static_cast<std::string>(secondInitMessage), referenceString);
-
-    secondInitMessage.read();
-    referenceString += "Simulating error in second script\n";
-    BOOST_CHECK_EQUAL(static_cast<std::string>(secondInitMessage), referenceString);
-
-    secondInitMessage.read();
-    referenceString += "!!! Dummy0 initialisation FAILED!";
-    BOOST_CHECK_EQUAL(static_cast<std::string>(secondInitMessage), referenceString);
+    produceErrorFile.seekp(0);
+    produceErrorFile << i << std::flush;
+    std::string referenceString =
+        "Simulating error in second script: " + std::to_string(i) + "\n!!! Dummy0 initialisation FAILED!";
+    CHECK_TIMEOUT((secondInitMessage.readLatest(), std::string(secondInitMessage)) == referenceString, 20000);
   }
 
   (void)std::filesystem::remove("produceDevice2InitError");
 
   // recovery
-  secondInitMessage.read(); // empty
-  secondInitMessage.read(); // script message
-  secondInitMessage.read(); // success message
-  BOOST_CHECK_EQUAL(
-      static_cast<std::string>(secondInitMessage), "just a second script\nDummy0 initialisation SUCCESS!");
+  std::string referenceString = "just a second script\nDummy0 initialisation SUCCESS!";
+  CHECK_TIMEOUT((secondInitMessage.readLatest(), std::string(secondInitMessage)) == referenceString, 20000);
   // at least three failure grace periods
   auto stopTime = std::chrono::steady_clock::now();
   BOOST_CHECK(std::chrono::duration_cast<std::chrono::seconds>(stopTime - startTime).count() >= 3);
+
+  (void)std::filesystem::remove("device1Init.success");
+  (void)std::filesystem::remove("continueDevice1Init");
+}
+
+/*********************************************************************************************************************/
+
+BOOST_FIXTURE_TEST_CASE(testLineByLineOutput, Fixture) {
+  std::ofstream produceErrorFile; // If the file exists, the script produces an error
+  produceErrorFile.open("produceDevice2InitSecondLine", std::ios::out);
+
+  // let script1 finish
+  std::ofstream continueFile;
+  continueFile.open("continueDevice1Init", std::ios::out);
+
+  testFacility.runApplication();
+
+  // testApp.dumpConnections();
+  auto secondInitMessage = testFacility.getScalar<std::string>("/Devices/Dummy0/secondInitScriptOutput");
+
+  std::string referenceString = "Just another output line...\n";
+  CHECK_TIMEOUT((secondInitMessage.readLatest(), std::string(secondInitMessage)) == referenceString, 20000);
+
+  (void)std::filesystem::remove("produceDevice2InitSecondLine");
+
+  referenceString = "Just another output line...\njust a second script\nDummy0 initialisation SUCCESS!";
+  CHECK_TIMEOUT((secondInitMessage.readLatest(), std::string(secondInitMessage)) == referenceString, 20000);
 
   (void)std::filesystem::remove("device1Init.success");
   (void)std::filesystem::remove("continueDevice1Init");
