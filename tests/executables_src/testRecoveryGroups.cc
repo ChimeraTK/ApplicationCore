136dd623203de3d827dceb8e6067c05647f21d2f
Martin Killenberg <martin.killenberg@desy.de>
2025-03-07 14:57:47 +0100

    fixup! feat: recovery groups

--- tests/executables_src/testRecoveryGroups.cc
+++ tests/executables_src/testRecoveryGroups.cc
@@ -24,6 +24,14 @@ namespace ctk = ChimeraTK;
 #include <cstdint>
 #include <string>
 
+// Helper class to have all variable names from a device prepended by the cdd/alias name
+// e.g. /Integers/unsigned32 from Use1 ends up in /Use1/Integers/unsigned32
+struct DeviceModuleWithPath : public ctk::ModuleGroup {
+  DeviceModuleWithPath(ModuleGroup* owner, std::string const& cdd)
+  : ModuleGroup(owner, cdd, ""), dev(this, cdd, "/somepath/dummyTrigger") {}
+  ctk::DeviceModule dev;
+};
+
 // A test application with 7 devices in 3 recovery groups.
 // It is used in all tests, and extended with initialisation handlers and further backends where needed.
 struct BasicTestApp : ctk::Application {
@@ -32,27 +40,13 @@ struct BasicTestApp : ctk::Application {
 
   ctk::SetDMapFilePath path{"recoveryGroups.dmap"};
 
-  struct DeviceModuleWithPath : public ctk::ModuleGroup {
-    DeviceModuleWithPath(
-        ModuleGroup* owner, std::string const& cdd_, std::function<void(ChimeraTK::Device&)> initHandler_ = nullptr)
-    : ModuleGroup(owner, cdd_, ""), cdd(cdd_), initHandler(std::move(initHandler_)) {}
-    std::string cdd;
-    std::function<void(ChimeraTK::Device&)> initHandler;
-    ctk::DeviceModule dev{this, cdd, "/somepath/dummyTrigger", initHandler};
-  };
-
-  // First recovery group: Two devices with one backend each, and a logical name map which uses them both.
+  // Recovery group: Two devices with one backend each, and a device which uses both of them
   DeviceModuleWithPath singleDev1{this, "Use1"};
   DeviceModuleWithPath singleDev2{this, "Use2"};
   DeviceModuleWithPath mappedDev12{this, "Use12"};
 
-  // Second recovery group with use3 and use4
+  // Use3 is in its own recovery "group"
   DeviceModuleWithPath singleDev3{this, "Use3"};
-  DeviceModuleWithPath singleDev4{this, "Use4"};
-  DeviceModuleWithPath mappedDev34{this, "Use34"};
-
-  // Use5 is in its own recovery "group"
-  DeviceModuleWithPath singleDev5{this, "Use5"};
 };
 
 /**********************************************************************************************************************/
@@ -65,128 +59,43 @@ struct Fixture {
 
   ctk::Device raw1{"Raw1"};
   ctk::Device raw2{"Raw2"};
-  ctk::Device raw3{"Raw3"};
-  ctk::Device raw4{"Raw4"};
-  ctk::Device raw5{"Raw5"};
 
   Fixture() { testFacility.runApplication(); };
 };
 
 /**********************************************************************************************************************/
 
-// Test that App1 actually has three different recovery groups: [Use1, Use2, Use12], [Use3, Use4, Use34] and [Use5]
+// Test that App1 actually has two different recovery groups: [Use1, Use2, Use12], [Use3]
 BOOST_FIXTURE_TEST_CASE(Test3RecoveryGroups, Fixture<BasicTestApp>) {
-  // wait until all devices are ok
-  for(auto const* dev : {"Use1", "Use2", "Use3", "Use4", "Use5", "Use12", "Use34"}) {
+  // Pre-condition: wait until all devices are ok
+  // Necessary because we are not using the testable mode
+  for(auto const* dev : {"Use1", "Use2", "Use3", "Use12"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
   }
 
-  // turn backend 3 into exception state
-  auto dummy3 = boost::dynamic_pointer_cast<ctk::ExceptionDummy>(raw3.getBackend());
-  dummy3->throwExceptionOpen = true;
-  dummy3->throwExceptionRead = true;
-
-  trigger.write();
-  // check that Use3, Use4 and Use34 are in error state, while the other devices are still working
-  for(auto const* dev : {"Use3", "Use4", "Use34"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
-  }
-
-  testFacility.writeScalar("Use1/Integers/signed32", 42);
-  CHECK_TIMEOUT(raw1.read<int32_t>("Integers/signed32") == 42, 10000);
-  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use1/status") == 0, 10000);
-
-  testFacility.writeScalar("/Use2/Integers/signed32", 43);
-  CHECK_TIMEOUT(raw2.read<int32_t>("Integers/signed32") == 43, 10000);
-  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use2/status") == 0, 10000);
-
-  testFacility.writeScalar<int16_t>("/Use12/Integers/signed16", 44);
-  CHECK_TIMEOUT(raw1.read<int16_t>("Integers/signed16") == 44, 10000);
-  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use12/status") == 0, 10000);
-
-  testFacility.writeScalar("/Use5/Integers/signed32", 45);
-  CHECK_TIMEOUT(raw5.read<int32_t>("Integers/signed32") == 45, 10000);
-  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use5/status") == 0, 10000);
-
-  // also set backend 1 into error state. Now only Use5 is working.
+  // Test preparation: turn backend 1 into exception state
   auto dummy1 = boost::dynamic_pointer_cast<ctk::ExceptionDummy>(raw1.getBackend());
   dummy1->throwExceptionOpen = true;
   dummy1->throwExceptionRead = true;
 
   trigger.write();
-  // All devices but Use5 are in error state
-  for(auto const* dev : {"Use1", "Use2", "Use12", "Use3", "Use4", "Use34"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
-  }
-  // Use5 is still working
-  testFacility.writeScalar("/Use5/Integers/signed32", 46);
-  CHECK_TIMEOUT(raw5.read<int32_t>("Integers/signed32") == 46, 10000);
-  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use5/status") == 0, 10000);
 
-  // Remove error condition on raw3
-  dummy3->throwExceptionOpen = false;
-  dummy3->throwExceptionRead = false;
-
-  // Wait for Use3, Use4 and use34 to recover, while Use1, Use2 and Use12 are still in error state
-  for(auto const* dev : {"Use3", "Use4", "Use34", "Use5"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
-  }
+  // The actual test: Check that Use1, Use2 and Use12 are in error state, while Use3 is still working
   for(auto const* dev : {"Use1", "Use2", "Use12"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
   }
+
+  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use3/status") == 0, 10000);
 
   // Remove error condition on raw1 and recover everything
   dummy1->throwExceptionOpen = false;
   dummy1->throwExceptionRead = false;
 
-  for(auto const* dev : {"Use1", "Use2", "Use12", "Use3", "Use4", "Use34", "Use5"}) {
+  for(auto const* dev : {"Use1", "Use2", "Use12", "Use3"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
   }
 }
 
-/**********************************************************************************************************************/
-
-struct TestApp2 : public BasicTestApp {
-  TestApp2() : BasicTestApp("twoRecoveryGroups") {}
-  ~TestApp2() override { shutdown(); }
-
-  // Add another device which "connects" the two recovery groups in App1.
-  // Now there is no device which has both the backends raw1 and raw4, but they are in the same
-  // recovery group.
-  DeviceModuleWithPath mappedDev23{this, "Use23"};
-};
-
-/**********************************************************************************************************************/
-
-BOOST_FIXTURE_TEST_CASE(Test2RecoveryGroups, Fixture<TestApp2>) {
-  // pre-condition: all devices OK
-  for(auto const* dev : {"Use1", "Use2", "Use12", "Use3", "Use4", "Use34", "Use5", "Use23"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
-  }
-
-  // Put dummy1 into an error state. Now the whole large recovery group (except for Use5) go into error state.
-  // In particular, also Use4, which is in no Device together with Use1.
-  auto dummy1 = boost::dynamic_pointer_cast<ctk::ExceptionDummy>(raw1.getBackend());
-  dummy1->throwExceptionOpen = true;
-  dummy1->throwExceptionRead = true;
-
-  trigger.write();
-  // All devices but Use5 are in error state
-  for(auto const* dev : {"Use1", "Use2", "Use12", "Use3", "Use4", "Use34", "Use23"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
-  }
-  CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use5/status") == 0, 10000);
-
-  // Recovery
-  dummy1->throwExceptionOpen = false;
-  dummy1->throwExceptionRead = false;
-
-  trigger.write();
-
-  for(auto const* dev : {"Use1", "Use2", "Use12", "Use3", "Use4", "Use34", "Use5", "Use23"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
-  }
-}
 /**********************************************************************************************************************/
 
 struct TestStepsApp : BasicTestApp {
@@ -347,63 +256,77 @@ BOOST_FIXTURE_TEST_CASE(TestInitFailure, Fixture<InitFailureApp>) {
 
 /**********************************************************************************************************************/
 
-BOOST_FIXTURE_TEST_CASE(TestRecoveryWriteFailure, Fixture<BasicTestApp>) {
+struct RecoveryFailureTestApp : ctk::Application {
+  explicit RecoveryFailureTestApp() : Application("RecoveryFailureTestApp") {}
+  ~RecoveryFailureTestApp() override { shutdown(); }
+
+  ctk::SetDMapFilePath path{"recoveryGroups.dmap"};
+
+  // recovery group with Use1 and Use2
+  DeviceModuleWithPath singleDev1{this, "Use1"};
+  DeviceModuleWithPath singleDev2{this, "Use2"};
+  DeviceModuleWithPath mappedDev12{this, "Use12ReadOnly"};
+};
+
+/**********************************************************************************************************************/
+
+BOOST_FIXTURE_TEST_CASE(TestRecoveryWriteFailure, Fixture<RecoveryFailureTestApp>) {
   // This test is checking that the error condition of a failure when writing the recovery accessors do
   // not confuse the barrier order and lock up the manager.
 
   // pre-condition: all (relevant) devices OK
-  for(auto const* dev : {"Use3", "Use4"}) {
+  for(auto const* dev : {"Use1", "Use2"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
   }
 
-  // Write something to Use3 so we can check when its recovery accessor writing is through.
-  // We take Use3 this time because it does not have an init handler, so the DeviceManager
+  // Write something to Use1 so we can check when its recovery accessor writing is through.
+  // We take Use1 this time because it does not have an init handler, so the DeviceManager
   // does not inadvertently close the device while we want to read it in the test.
-  // In addition, Use34 only has read-only registers, so it does not cause errors in recovery,
-  // which would set the device 3 back into error state.
-  testFacility.writeScalar<uint32_t>("/Use3/Integers/unsigned32", 18);
-  CHECK_TIMEOUT(raw3.read<uint32_t>("Integers/unsigned32") == 18, 10000);
+  // In addition, Use12ReadOnly has only read-only registers, so it does not cause errors in recovery,
+  // which would set the device 1 back into error state.
+  testFacility.writeScalar<uint32_t>("/Use1/Integers/unsigned32", 18);
+  CHECK_TIMEOUT(raw1.read<uint32_t>("Integers/unsigned32") == 18, 10000);
 
   // create an error condition which also thrown when writing (the recovery accessors)
-  auto dummy4 = boost::dynamic_pointer_cast<ctk::ExceptionDummy>(raw4.getBackend());
+  auto dummy4 = boost::dynamic_pointer_cast<ctk::ExceptionDummy>(raw2.getBackend());
   dummy4->throwExceptionOpen = true;
   dummy4->throwExceptionRead = true;
   dummy4->throwExceptionWrite = true;
   trigger.write();
 
-  // Wait until the error has been detected, then clean the recovery value on Use3 and
+  // Wait until the error has been detected, then clean the recovery value on Use1 and
   // resolve the open/read errors, but keep the write error.
-  for(auto const* dev : {"Use3", "Use4"}) {
+  for(auto const* dev : {"Use1", "Use2"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
   }
-  // Get the status accessors to check that Use3 does not report ready, although it
+  // Get the status accessors to check that Use1 does not report ready, although it
   // got through with its recovery.
-  auto use3Status = testFacility.getScalar<int32_t>("Devices/Use3/status");
-  use3Status.readLatest(); // just empty the queue
-  auto use3BecameFunctional = testFacility.getVoid("Devices/Use3/deviceBecameFunctional");
-  use3BecameFunctional.readLatest(); // just empty the queue
+  auto Use1Status = testFacility.getScalar<int32_t>("Devices/Use1/status");
+  Use1Status.readLatest(); // just empty the queue
+  auto Use1BecameFunctional = testFacility.getVoid("Devices/Use1/deviceBecameFunctional");
+  Use1BecameFunctional.readLatest(); // just empty the queue
 
   // now clear the opening error.
   dummy4->throwExceptionOpen = false;
   dummy4->throwExceptionRead = false;
 
-  // Wait until we can read/write from raw3 without error.
-  CHECK_TIMEOUT(raw3.isFunctional(), 10000);
-  raw3.write("Integers/unsigned32", 0);
-  // Wait until we have seen the recovery section of Use3 run through, then clear it and wait again.
-  // Like this we make sure that the error condition in Use4 has been hit at least once.
-  CHECK_TIMEOUT(raw3.read<uint32_t>("Integers/unsigned32") == 18, 10000);
+  // Wait until we can read/write from raw1 without error.
+  CHECK_TIMEOUT(raw1.isFunctional(), 10000);
+  raw1.write("Integers/unsigned32", 0);
+  // Wait until we have seen the recovery section of Use1 run through, then clear it and wait again.
+  // Like this we make sure that the error condition in Use2 has been hit at least once.
+  CHECK_TIMEOUT(raw1.read<uint32_t>("Integers/unsigned32") == 18, 10000);
 
-  raw3.write("Integers/unsigned32", 0);
-  CHECK_TIMEOUT(raw3.read<uint32_t>("Integers/unsigned32") == 18, 10000);
+  raw1.write("Integers/unsigned32", 0);
+  CHECK_TIMEOUT(raw1.read<uint32_t>("Integers/unsigned32") == 18, 10000);
 
   // No changing status has been reported in the mean time
-  BOOST_TEST(!use3Status.readNonBlocking());
-  BOOST_TEST(!use3BecameFunctional.readNonBlocking());
+  BOOST_TEST(!Use1Status.readNonBlocking());
+  BOOST_TEST(!Use1BecameFunctional.readNonBlocking());
 
   // Finally, resolve the error condition and wait until everything recovers.
   dummy4->throwExceptionWrite = false;
-  for(auto const* dev : {"Use3", "Use4"}) {
+  for(auto const* dev : {"Use1", "Use2"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
   }
 }
