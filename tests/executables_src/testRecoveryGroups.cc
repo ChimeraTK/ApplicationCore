22bee98428e64296c0a67095d8e46f4d7f11d106
Martin Killenberg <martin.killenberg@desy.de>
2025-03-13 17:06:07 +0100

    fixup! feat: recovery groups

--- tests/executables_src/testRecoveryGroups.cc
+++ tests/executables_src/testRecoveryGroups.cc
@@ -1,5 +1,8 @@
 // SPDX-FileCopyrightText: Deutsches Elektronen-Synchrotron DESY, MSK, ChimeraTK Project <chimeratk-support@desy.de>
 // SPDX-License-Identifier: LGPL-3.0-or-later
+#include <ChimeraTK/Device.h>
+
+#include <boost/smart_ptr/make_shared_object.hpp>
 #define BOOST_TEST_MODULE testRecoveryGroups
 
 #include "Application.h"
@@ -13,6 +16,7 @@
 #include <ChimeraTK/cppext/finally.hpp>
 #include <ChimeraTK/Exception.h>
 #include <ChimeraTK/ExceptionDummyBackend.h>
+#include <ChimeraTK/LogicalNameMappingBackend.h>
 #include <ChimeraTK/NDRegisterAccessor.h>
 #include <ChimeraTK/ScalarRegisterAccessor.h>
 #include <ChimeraTK/VoidRegisterAccessor.h>
@@ -33,6 +37,33 @@ struct DeviceModuleWithPath : public ctk::ModuleGroup {
   : ModuleGroup(owner, cdd, ""), dev(this, cdd, "/somepath/dummyTrigger") {}
   ctk::DeviceModule dev;
 };
+
+// Test backend which counts the number of open() calls and allows to block write operations.
+struct RecoveryGroupTestBackend : public ChimeraTK::LogicalNameMappingBackend {
+  using LogicalNameMappingBackend::LogicalNameMappingBackend;
+  std::atomic<size_t> openCounter{0};
+  void open() override {
+    ++openCounter;
+    ChimeraTK::LogicalNameMappingBackend::open();
+  }
+
+  // NOLINTNEXTLINE performance-unnecessary-value-param (signature required like this by BackendFactory)
+  static boost::shared_ptr<DeviceBackend> creatorFunction(std::string, std::map<std::string, std::string> parameters) {
+    auto ptr = boost::make_shared<RecoveryGroupTestBackend>(parameters["map"]);
+    parameters.erase(parameters.find("map"));
+    ptr->_parameters = parameters;
+    return boost::static_pointer_cast<DeviceBackend>(ptr);
+  }
+
+  struct Registerer {
+    Registerer() {
+      ctk::BackendFactory::getInstance().registerBackendType(
+          "RecoveryGroupTestBackend", RecoveryGroupTestBackend::creatorFunction);
+    }
+  };
+};
+
+static RecoveryGroupTestBackend::Registerer testBackendRegisterer;
 
 // A test application with 4 devices in 2 recovery groups.
 // It is used in most tests, and extended with initialisation handlers where needed.
@@ -68,14 +99,14 @@ struct Fixture {
 /**********************************************************************************************************************/
 
 /**
- * \anchor testExceptionHandling_a_5_1 \ref exceptionHandling_b_5_1 "A.5.1"
+ * \anchor testExceptionHandling_a_5 \ref exceptionHandling_a_5 "A.5"
  * DeviceManagers with at least one common involved backend ID (see DeviceBackend::getInvolvedBackendIDs()) form a
  * recovery group. They collectively see exceptions and are recovered together.
  *
- * \anchor testExceptionHandling_a_5_2 \ref exceptionHandling_b_5_2 "A.5.2"
+ * \anchor testExceptionHandling_a_5_1 \ref exceptionHandling_a_5_1 "A.5.1"
  *  Recovery groups which don't share any backend IDs behave independently.
  *
- * Note: the tests are done together because test A.5.2 requires exactly the same lines of code as the A.5.1 test
+ * Note: the tests are done together because test A.5.1 requires exactly the same lines of code as the A.5 test
  */
 BOOST_FIXTURE_TEST_CASE(TestRecoveryGroups, Fixture<BasicTestApp>) {
   // Pre-condition: wait until all devices are ok
@@ -91,13 +122,13 @@ BOOST_FIXTURE_TEST_CASE(TestRecoveryGroups, Fixture<BasicTestApp>) {
 
   trigger.write();
 
-  // The actual test A.5.1: Check that Use1, Use2 and Use12 are in the same recovery group and thus have seen the error.
-  // Requirement for A.5.2
+  // The actual test A.5: Check that Use1, Use2 and Use12 are in the same recovery group and thus have seen the error.
+  // Requirement for A.5.1
   for(auto const* dev : {"Use1", "Use2", "Use12"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
   }
 
-  // Test A.5.2: Use3 is in a different recovery and still OK
+  // Test A.5.1: Use3 is in a different recovery and still OK
   CHECK_TIMEOUT(testFacility.readScalar<int>("Devices/Use3/status") == 0, 10000);
 
   // Remove error condition on raw1 and recover everything
@@ -112,7 +143,7 @@ BOOST_FIXTURE_TEST_CASE(TestRecoveryGroups, Fixture<BasicTestApp>) {
 /**********************************************************************************************************************/
 
 /**
- * \anchor testExceptionHandling_a_5_3 \ref exceptionHandling_b_5_1 "A.5.3"
+ * \anchor testExceptionHandling_a_5_2 \ref exceptionHandling_a_5_2 "A.5.2"
  * Two DeviceManagers which are not sharing any involved backend IDs will end up in the same recovery group if there is
  * one other DeviceManager sharing an involved backend ID with each of them.
  */
@@ -266,17 +297,37 @@ BOOST_FIXTURE_TEST_CASE(TestRecoveryStepInitHandlers, Fixture<BlockInitTestApp>)
 /**********************************************************************************************************************/
 struct InitFailureApp : BasicTestApp {
   explicit InitFailureApp() : BasicTestApp("InitFailureApp") {
-    singleDev2.dev.addInitialisationHandler([&](ctk::Device&) { init2(); });
+    singleDev1.dev.addInitialisationHandler([&](ctk::Device&) { init(); });
+    singleDev2.dev.addInitialisationHandler([&](ctk::Device&) { init(); });
   }
   ~InitFailureApp() override { shutdown(); }
 
   // InitFunction to raise an error
-  std::atomic<bool> failInit2{false};
-  std::atomic<size_t> failCounter{0};
-  void init2() {
-    if(failInit2) {
-      throw ctk::runtime_error("Intentional failure " + std::to_string(++failCounter) + " in init2()");
+  std::atomic<bool> blockInitOnce{false}; // block the execution of at the start if all init handlers
+  std::barrier<> blockInitArrivedBarrier{2};
+  std::barrier<> blockInitContinueBarrier{2};
+  std::atomic<bool> failInit{false};
+  std::atomic<size_t> initCounter{0};
+  std::atomic<size_t> initFailCounter{0};
+  std::atomic<size_t> initSuccessCounter{0};
+  std::barrier<> aboutToFail{2};     // notify the test where we are. It has to do some checks
+  std::barrier<> proceedWithFail{2}; // wait for the test to complete its checks
+  void init() {
+    if(blockInitOnce) {
+      blockInitOnce = false;
+      (void)blockInitArrivedBarrier.arrive();     // notify the test
+      blockInitContinueBarrier.arrive_and_wait(); // only continue when testing is done
     }
+    if(failInit) {
+      if(++initCounter == 2) {
+        // This branch will be only hit once because the counter is higher afterwards.
+        (void)aboutToFail.arrive();        // notify the test that it can do the preparation
+        proceedWithFail.arrive_and_wait(); // wait for the test to complete the preparation
+        std::cout << "TRHOWING!!!" << std::endl;
+        throw ctk::runtime_error("Intentional failure " + std::to_string(++initFailCounter) + " in init2()");
+      }
+    }
+    ++initSuccessCounter;
   }
 };
 
@@ -287,43 +338,96 @@ struct InitFailureApp : BasicTestApp {
  * group restart the recovery procedure after the POST-INIT-HANDLER barrier.
  */
 BOOST_FIXTURE_TEST_CASE(TestInitFailure, Fixture<InitFailureApp>) {
-  // This test is also checking that the error condition of a failure in the init handler does
-  // not confuse the barrier order and lock up the manager.
+  // Side effect: This test is also checking that the error condition of a failure in the init handler does
+  // not confuse the barrier order and lock up the manager (basically error handling smoke test).
+
+  // This test contains three checks:
+  //  1. *All* device managers restart the recovery.
+  //  2. The recovery restarts with *open* (not only the init step is repeated).
+  //  3. The recovery happens *after the POST-INIT-HANDLER barrier*.
 
   // pre-condition: all (relevant) devices OK
   for(auto const* dev : {"Use1", "Use2"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
   }
 
+  // preparation for check 3: Use1 is at the POST-INIT-HANDLER barrier, i.e. init handler is through, recovering write
+  // values is not.
+  testFacility.writeScalar<uint32_t>("/Use1/Integers/unsigned32", 17); // written by recovery write
+  testFacility.writeScalar<uint32_t>("/Use2/Integers/unsigned32", 18); // written by recovery write
+  testFacility.writeScalar<int16_t>("/Use12/Integers/signed16", 19);   // written by recovery write
+  // Wait until the value arrived at the device, the overwrite
+  CHECK_TIMEOUT(raw1.read<uint32_t>("/Integers/unsigned32") == 17, 10000);
+  CHECK_TIMEOUT(raw2.read<uint32_t>("/Integers/unsigned32") == 18, 10000);
+  CHECK_TIMEOUT(raw1.read<int16_t>("/Integers/signed16") == 19, 10000);
+  raw1.write<uint32_t>("/Integers/unsigned32", 13);
+  raw2.write<uint32_t>("/Integers/unsigned32", 14);
+  raw1.write<int16_t>("/Integers/signed16", 15);
+
   // Set the init script to fail and trigger an error condition.
-  testApp.failInit2 = true;
+  testApp.failInit = true;
+  testApp.initSuccessCounter = 0;
+  testApp.singleDev1.dev.reportException("reported from test");
 
-  auto dummy1 = boost::dynamic_pointer_cast<ctk::ExceptionDummy>(raw1.getBackend());
-  dummy1->throwExceptionOpen = true;
-  dummy1->throwExceptionRead = true;
-  trigger.write();
-  // Wait until the error has been detected, then resolve it.
-  for(auto const* dev : {"Use1", "Use2"}) {
-    CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 1, 10000);
-  }
+  testApp.aboutToFail.arrive_and_wait();
 
-  dummy1->throwExceptionOpen = false;
-  dummy1->throwExceptionRead = false;
+  // Check 3 part 1: One of the init handlers increased the initSuccessCounter, so we know it has
+  // run and due to the sleeps, we can be pretty sure it has arrived at the POST-INIT-HANDLER barrier.
+  BOOST_CHECK(testApp.initSuccessCounter == 1);
 
-  // Wait until the failure counter is larger than 1 , i.e. the DeviceManager has actually failed at the place we
-  // want, and the recovery process has restarted after it at least once (by checking that it failed twice).
-  // FIXME: This does not check that
-  // - all device managers retry
-  // - the recovery restarts with open (could only be the init step)
-  // - the recovery only happens after the init barrier
-  BOOST_CHECK(false);
-  CHECK_TIMEOUT(testApp.failCounter > 1, 10000);
+  // Preparation. At this point we know that
+  // - One of the init handlers has run through.
+  // - The other init handler is waiting in the init handler
+  // - The successful init handler has a higher open count because the device is re-opened after the init handler
+  // But we don't know which device is in which state.
+  // So we store both open counters.
+  auto testLmap1 = boost::dynamic_pointer_cast<RecoveryGroupTestBackend>(
+      testApp.singleDev1.dev.getDeviceManager().getDevice().getBackend());
+  auto testLmap2 = boost::dynamic_pointer_cast<RecoveryGroupTestBackend>(
+      testApp.singleDev2.dev.getDeviceManager().getDevice().getBackend());
+  auto testLmap12 = boost::dynamic_pointer_cast<RecoveryGroupTestBackend>(
+      testApp.mappedDev12.dev.getDeviceManager().getDevice().getBackend());
+  size_t openCount1 = testLmap1->openCounter;
+  size_t openCount2 = testLmap2->openCounter;
+  size_t openCount12 = testLmap12->openCounter;
+
+  std::cout << "1 openCounter " << testLmap1->openCounter << ", 2 openCounter " << testLmap2->openCounter
+            << ", 12 openCounter " << testLmap12->openCounter << std::endl;
+  // Also block the execution of newly starting init handlers so we know that at this point only
+  // the open step has happened. This simplifies testing.
+  testApp.blockInitOnce = true;
+
+  // now make the second init handler throw
+  (void)testApp.proceedWithFail.arrive();
+
+  // Check 1 and 2: *All* DeviceManagers have *restarted* the  recovery procedure.
+  // The restart of the recovery procedure is detected by looking at the open counter.
+  testApp.blockInitArrivedBarrier.arrive_and_wait();
+  BOOST_TEST(testLmap1->openCounter == openCount1 + 1);
+  BOOST_TEST(testLmap2->openCounter == openCount2 + 1);
+  BOOST_TEST(testLmap12->openCounter == openCount12 + 1);
+
+  std::cout << "1 openCounter " << testLmap1->openCounter << ", 2 openCounter " << testLmap2->openCounter
+            << ", 12 openCounter " << testLmap12->openCounter << std::endl;
+
+  // Check 3: The recover actually restarted after the POST-INIT-HANDLER and no write recovery was done.
+  // We know that one of the init handlers is blocking, so one of the devices is closed,
+  // but no futher close will happen while the init handler is blocking.
+  // Just reopen all devices.
+  raw1.open();
+  raw2.open();
+  BOOST_TEST(raw1.read<uint32_t>("/Integers/unsigned32") == 13);
+  BOOST_TEST(raw2.read<uint32_t>("/Integers/unsigned32") == 14);
+  BOOST_TEST(raw1.read<int16_t>("/Integers/signed16") == 15);
 
   // Resolve the error condition and wait until everything has recovered.
-  testApp.failInit2 = false;
+  testApp.failInit = false;
+  (void)testApp.blockInitContinueBarrier.arrive();
   for(auto const* dev : {"Use1", "Use2"}) {
     CHECK_TIMEOUT(testFacility.readScalar<int>(std::string("Devices/") + dev + "/status") == 0, 10000);
   }
+  std::cout << "1 openCounter " << testLmap1->openCounter << ", 2 openCounter " << testLmap2->openCounter
+            << ", 12 openCounter " << testLmap12->openCounter << std::endl;
 }
 
 /**********************************************************************************************************************/
@@ -467,7 +571,7 @@ struct IncompleteRecoveryTestApp : ctk::Application {
 /**********************************************************************************************************************/
 
 /**
- * \anchor testExceptionHandling_b_3_1_2_2 \ref exceptionHandling_b_3_1_2_2 "B.3.1.2.2" The application terminates
+ * \anchor testExceptionHandling_b_3_3 \ref exceptionHandling_b_3_3 "B.3.3" The application terminates
  * cleanly, even if the recovery is waiting at one of the barriers mentioned in \ref b_3_1 "3.1"
  */
 BOOST_AUTO_TEST_CASE(TestIncompleteRecovery) {
@@ -486,6 +590,8 @@ BOOST_AUTO_TEST_CASE(TestIncompleteRecovery) {
 
     // Wait until the init handler which will throw told us it has reached that point, so we don't end the application
     // scope before the test is sensitive.
+    // The second init handler which is run does the blocking, and sleeps a bit before arriving here, so we are pretty
+    // sure that the other init handler has reached the barrier.
     testApp.aboutToThrow.arrive_and_wait();
   }
   // The actual test: We reached this point, the test did not block
