520ee297b37f040fb6f0dd8ec0fd90bd4dcd3f46
Jens Georg <jens.georg@desy.de>
2025-05-06 11:54:26 +0200

    chore: Port to newer ApplicationCore

--- example_status_monitor/demoStatusMonitor.cc
+++ example_status_monitor/demoStatusMonitor.cc
@@ -21,7 +21,7 @@ struct SimulationModule : public ctk::ApplicationModule {
   /**The value to be monitored.*/
   ctk::ScalarOutput<double> temperature{this, "temperature", "degC", "simulated temperature"};
 
-  void mainLoop() {
+  void mainLoop() override {
     /**Intialize temperature.*/
     temperature = 0;
     temperature.write();
@@ -48,17 +48,8 @@ struct SimulationModule : public ctk::ApplicationModule {
 };
 
 struct ExampleApp : public ctk::Application {
-  ExampleApp() : Application("exampleApp") {
-    // show how it looks in the application (C++ hierarchy)
-    dump();
-
-    // show how it looks on the cs side (virtual hierarchy)
-    // cs.dump();
-
-    // show how it is connected
-    dumpConnections();
-  }
-  ~ExampleApp() { shutdown(); }
+  ExampleApp() : Application("exampleApp") { }
+  ~ExampleApp() override { shutdown(); }
 
   // Create an instance of the simulation module. We name it "Simulation".
   // There will be a variable /Simulation/temperature from this.
@@ -82,43 +73,7 @@ struct ExampleApp : public ctk::Application {
   } simulationGroup{this, "Simulation", ""};
 
   ctk::ConfigReader config{this, "Config", "demoStatusMonitor_config.xml"};
-  // ctk::ControlSystemModule cs;
-  // void defineConnections();
 };
 
 ExampleApp theExampleApp;
 
-// void ExampleApp::defineConnections() {
-//   // Usually you set the dmap file here. This example does not have one.
-
-//  // Connect everything in the app to the cs. This makes the connection of temperature from Simulation to the input of
-//  // the monitor because they are the same variable in the CS module. Also the disable variable if the monitor is
-//  // connected to the CS. Try what happens if you disable the monitor.
-//  findTag(".*").connectTo(cs);
-
-//  /* The trick of connecting the temperature automatically only worked because we put the temperatureMonitor into the
-//   * correct place in the hierarchy by putting it into the variable group "Simulation". However, the threshold
-//   * parameters inside the monitor are not connected yet.
-//   *
-//   * When connecting the app, the config created the following variables:
-//   * /Config/TemperatureMonitor/lowerWarningThreshold
-//   * /Config/TemperatureMonitor/upperWarningThreshold
-//   * /Config/TemperatureMonitor/lowerFaultThreshold
-//   * /Config/TemperatureMonitor/upperFaultThreshold
-//   */
-
-//  // Now we connect the parameters of the temperature monitor to the control system, right into the Config directory so
-//  // the variable names match. Like this the parameters are connected to the values coming from the configuration.
-//  findTag("CONFIG").flatten().connectTo(cs["Config"]["TemperatureMonitor"]);
-
-//  // FIXME: At this point a status aggregator would connect everything with tag STATUS
-
-//  // show how it looks in the application (C++ hierarchy)
-//  dump();
-
-//  // show how it looks on the cs side (virtual hierarchy)
-//  cs.dump();
-
-//  // show how it is connected
-//  dumpConnections();
-//}
