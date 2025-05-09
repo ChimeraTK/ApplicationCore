e91bd515af4ec3343f380aeaebd79cb039ba092c
Martin Hierholzer <martin.hierholzer@desy.de>
2025-05-09 10:35:22 +0200

    fix: for earlier regression fix for recovery groups

--- src/TestableMode.cc
+++ src/TestableMode.cc
@@ -99,19 +99,11 @@ namespace ChimeraTK::detail {
     }
 
     // debug output if enabled (also prevent spamming the same message)
-    if(_enableDebug && _repeatingMutexOwner == 0) { // LCOV_EXCL_LINE
-                                                    // (only cout)
-      logger(Logger::Severity::debug, "TestableMode")
+    if(_enableDebug) {                                                  // LCOV_EXCL_LINE (only cout)
+      logger(Logger::Severity::debug, "TestableMode")                   // LCOV_EXCL_LINE (only cout)
           << "Application::testableModeLock(): Thread " << threadName() // LCOV_EXCL_LINE (only cout)
           << " tries to obtain lock for " << name;                      // LCOV_EXCL_LINE (only cout)
     } // LCOV_EXCL_LINE (only cout)
-
-    // if last lock was obtained repeatedly by the same thread, sleep a short time
-    // before obtaining the lock to give the other threads a chance to get the
-    // lock first
-    if(_repeatingMutexOwner > 0) {
-      usleep(10000);
-    }
 
     // obtain the lock
     boost::thread::id lastSeen_lastOwner = _lastMutexOwner;
@@ -121,84 +113,25 @@ namespace ChimeraTK::detail {
     if(!success) {
       if(currentLastOwner != lastSeen_lastOwner) {
         lastSeen_lastOwner = currentLastOwner;
+        usleep(10000);
         goto repeatTryLock;
       }
-
-      std::cerr << "testableModeLock(): Thread " << threadName()                // LCOV_EXCL_LINE
-                << " could not obtain lock for 30 seconds, presumably because " // LCOV_EXCL_LINE
-                << threadName(_lastMutexOwner) << " [" << pthreadId(_lastMutexOwner) << "] does not release it."
-                << std::endl; // LCOV_EXCL_LINE
-
-      // throw a specialised exception to make sure whoever catches it really knows what he does...
-      terminateTestStalled(); // LCOV_EXCL_LINE
+      std::cerr << "testableModeLock(): Thread " << threadName()                     // LCOV_EXCL_LINE
+                << " could not obtain lock for at least 30 seconds, presumably because "      // LCOV_EXCL_LINE
+                << threadName(_lastMutexOwner) << " [" << pthreadId(_lastMutexOwner) // LCOV_EXCL_LINE
+                << "] does not release it."                                          // LCOV_EXCL_LINE
+                << std::endl;                                                        // LCOV_EXCL_LINE
+      terminateTestStalled();                                                        // LCOV_EXCL_LINE
     } // LCOV_EXCL_LINE
 
-    // check if the last owner of the mutex was this thread, which may be a hint
-    // that no other thread is waiting for the lock
-    if(currentLastOwner == boost::this_thread::get_id()) {
-      // debug output if enabled
-      if(_enableDebug && _repeatingMutexOwner == 0) {                       // LCOV_EXCL_LINE
-                                                                            // (only cout)
-        std::cerr << "testableModeLock(): Thread " << threadName()          // LCOV_EXCL_LINE (only cout)
-                  << " repeatedly obtained lock successfully for "          // LCOV_EXCL_LINE (only cout)
-                  << name                                                   // LCOV_EXCL_LINE (only cout)
-                  << ". Further messages will be suppressed." << std::endl; // LCOV_EXCL_LINE (only cout)
-      } // LCOV_EXCL_LINE (only cout)
+    _lastMutexOwner = boost::this_thread::get_id();
 
-      // increase counter for stall detection
-      _repeatingMutexOwner++;
-
-      // detect stall: if the same thread got the mutex with no other thread
-      // obtaining it in between for one second, we assume no other thread is able
-      // to process data at this time. The test should fail in this case
-      if(_repeatingMutexOwner > 100) {
-        // print an informative message first, which lists also all variables
-        // currently containing unread data.
-        std::cerr << "*** Tests are stalled due to data which has been sent but not received.\n";
-        std::cerr
-            << "    The following variables still contain unread values or had data loss due to a queue overflow:\n";
-        for(auto& pair : _variables) {
-          const auto& variable = pair.second;
-          if(variable.counter > 0) {
-            std::cerr << "    - " << variable.name << " [" << variable.processVariable->getId() << "]";
-            // check if process variable still has data in the queue
-            try {
-              if(variable.processVariable->readNonBlocking()) {
-                std::cerr << " (unread data in queue)";
-              }
-              else {
-                std::cerr << " (data loss)";
-              }
-            }
-            catch(ChimeraTK::logic_error&) {
-              // if we receive a logic_error in readNonBlocking() it just means
-              // another thread is waiting on a TransferFuture of this variable,
-              // and we actually were not allowed to read...
-              std::cerr << " (data loss)";
-            }
-            std::cerr << "\n";
-          }
-        }
-        std::cerr << "(end of list)\n";
-        // Check for modules waiting for initial values (prints nothing if there are no such modules)
-        Application::getInstance()._circularDependencyDetector.printWaiters(std::cerr);
-        // throw a specialised exception to make sure whoever catches it really knows what he does...
-        terminateTestStalled();
-      }
-    }
-    else {
-      // last owner of the mutex was different: reset the counter and store the
-      // thread id
-      _repeatingMutexOwner = 0;
-      _lastMutexOwner = boost::this_thread::get_id();
-
-      // debug output if enabled
-      if(_enableDebug) { // LCOV_EXCL_LINE (only cout)
-        logger(Logger::Severity::debug, "TestableMode")
-            << "TestableMode::lock(): Thread " << threadName() // LCOV_EXCL_LINE (only cout)
-            << " obtained lock successfully for " << name;     // LCOV_EXCL_LINE (only cout)
-      } // LCOV_EXCL_LINE (only cout)
-    }
+    // debug output if enabled
+    if(_enableDebug) {                                       // LCOV_EXCL_LINE (only cout)
+      logger(Logger::Severity::debug, "TestableMode")        // LCOV_EXCL_LINE (only cout)
+          << "TestableMode::lock(): Thread " << threadName() // LCOV_EXCL_LINE (only cout)
+          << " obtained lock successfully for " << name;     // LCOV_EXCL_LINE (only cout)
+    } // LCOV_EXCL_LINE (only cout)
   }
 
   /********************************************************************************************************************/
@@ -207,14 +140,11 @@ namespace ChimeraTK::detail {
     if(not _enabled) {
       return;
     }
-    if(_enableDebug &&
-        (not _repeatingMutexOwner                                                     // LCOV_EXCL_LINE (only cout)
-            || boost::thread::id(_lastMutexOwner) != boost::this_thread::get_id())) { // LCOV_EXCL_LINE (only cout)
-      logger(Logger::Severity::debug, "TestableMode")
+    if(_enableDebug) {                                         // LCOV_EXCL_LINE (only cout)
+      logger(Logger::Severity::debug, "TestableMode")          // LCOV_EXCL_LINE (only cout)
           << "TestableMode::unlock(): Thread " << threadName() // LCOV_EXCL_LINE (only cout)
           << " releases lock for " << name;                    // LCOV_EXCL_LINE (only cout)
-    }
-    // LCOV_EXCL_LINE (only cout)
+    } // LCOV_EXCL_LINE (only cout)
     getLockObject().unlock();
   }
 
@@ -231,7 +161,8 @@ namespace ChimeraTK::detail {
     // let the application run until it has processed all data (i.e. the semaphore
     // counter is 0)
     size_t oldCounter = 0;
-    while(_counter > 0 || (waitForDeviceInitialisation && _deviceInitialisationCounter > 0)) {
+    auto t0 = std::chrono::steady_clock::now();
+    while(true) {
       if(_enableDebug && (oldCounter != _counter)) { // LCOV_EXCL_LINE (only cout)
         logger(Logger::Severity::debug, "TestableMode")
             << "Application::stepApplication(): testableMode.counter = " << _counter; // LCOV_EXCL_LINE (only cout)
@@ -240,6 +171,49 @@ namespace ChimeraTK::detail {
       unlock("stepApplication");
       boost::this_thread::yield();
       lock("stepApplication", false);
+      if(_counter > 0 || (waitForDeviceInitialisation && _deviceInitialisationCounter > 0)) {
+        usleep(1000);
+
+        // If the application does not finish data processing (and hence counters will stay > 0), assume the test
+        // is stalled and terminate the test.
+        if(std::chrono::steady_clock::now() - t0 > std::chrono::seconds(30)) {
+          // print an informative message first, which lists also all variables
+          // currently containing unread data.
+          std::cerr << "*** Tests are stalled due to data which has been sent but not received.\n";
+          std::cerr
+              << "    The following variables still contain unread values or had data loss due to a queue overflow:\n";
+          for(auto& pair : _variables) {
+            const auto& variable = pair.second;
+            if(variable.counter > 0) {
+              std::cerr << "    - " << variable.name << " [" << variable.processVariable->getId() << "]";
+              // check if process variable still has data in the queue
+              try {
+                if(variable.processVariable->readNonBlocking()) {
+                  std::cerr << " (unread data in queue)";
+                }
+                else {
+                  std::cerr << " (data loss)";
+                }
+              }
+              catch(ChimeraTK::logic_error&) {
+                // if we receive a logic_error in readNonBlocking() it just means
+                // another thread is waiting on a TransferFuture of this variable,
+                // and we actually were not allowed to read...
+                std::cerr << " (data loss)";
+              }
+              std::cerr << "\n";
+            }
+          }
+          std::cerr << "(end of list)\n";
+          // Check for modules waiting for initial values (prints nothing if there are no such modules)
+          Application::getInstance()._circularDependencyDetector.printWaiters(std::cerr);
+          // throw a specialised exception to make sure whoever catches it really knows what he does...
+          terminateTestStalled();
+        }
+      }
+      else {
+        break;
+      }
     }
   }
 
